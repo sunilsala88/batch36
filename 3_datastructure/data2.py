40caa4798bc7f9901c0d0452e3aac8ea8d275703
sunilsala88 <sunilsala88@gmail.com>
2026-02-01 15:26:08 +0530

    feb-1-9

--- 3_datastructure/data2.py
+++ 3_datastructure/data2.py
@@ -13,3 +13,20 @@ print(d1)
 
 d1.update({'banknifty':908})
 print(d1)
+
+#update existing
+
+d1['nifty50']=900 #old
+print(d1)
+
+d1.update({'banknifty':909})
+print(d1)
+
+#remove
+
+d1.pop('amzn')
+print(d1)
+
+a=d1.popitem()
+print(d1)
+print(a)
