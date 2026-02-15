0592543fda956907328cafc54033ea02ed38ee58
sunilsala88 <sunilsala88@gmail.com>
2026-02-15 15:08:40 +0530

    feb-15-5

--- 5_loops/while1.py
+++ 5_loops/while1.py
@@ -2,9 +2,38 @@
 number=1
 
 while True:
-    if number==100:
+    if number==5:
         break
     print(number)
     number=number+1
 
 print('last line')
+
+
+fib=[0,1]
+num1=fib[0]
+num2=fib[1]
+
+for i in range(8):
+    current=num1+num2
+    fib.append(current)
+    num1=num2
+    num2=current
+print(fib)
+
+
+fib=[0,1]
+num1=fib[0]
+num2=fib[1]
+count=0
+while True:
+    if count==8:
+        break
+    count=count+1
+    current=num1+num2
+    fib.append(current)
+    num1=num2
+    num2=current
+print(fib)
+
+l2=[22,33,44,55,66]
