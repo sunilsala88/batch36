62d77ec2cc06956ac7060c1977524b530e963229
sunilsala88 <sunilsala88@gmail.com>
2026-02-28 14:46:33 +0530

    feb28-1

---
+++ 7_files_exception/file1.py
@@ -0,0 +1,18 @@
+
+
+#write
+# f1=open('demo1.txt','w')
+# f1.write('this is my 2nd line')
+# f1.close()
+
+
+#read
+# f2=open('demo1.txt','r')
+# data=f2.read()
+# print(data)
+# f2.close()
+
+#append
+f3=open('demo1.txt','a')
+f3.write('\n fourth line')
+f3.close()
