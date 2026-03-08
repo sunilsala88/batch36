f9eebd2691ea492a3227a4fa39d8814fb10d8ad3
sunilsala88 <sunilsala88@gmail.com>
2026-03-08 15:17:53 +0530

    mar8-3

--- 9_module/file2.py
+++ 9_module/file2.py
@@ -18,3 +18,40 @@ print(p1.price)
 from file1 import *
 print(name)
 print(age)
+
+#inbuild library
+
+import random
+
+print(random.randint(1,100))
+
+import math
+
+print(math.sqrt(16))
+
+import time
+print('first')
+time.sleep(2)
+print('second')
+
+# num=0
+# while True:
+#     print(num)
+#     num=num+1
+#     time.sleep(1)
+
+import os
+print(os.getcwd())
+
+import sys
+
+num=0
+while True:
+    if num==5:
+        sys.exit()
+        # break
+    print(num)
+    num=num+1
+    time.sleep(1)
+
+print('last line')
