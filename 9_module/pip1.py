c51c1b25d067b5f3647a620265a877d81385e9e0
sunilsala88 <sunilsala88@gmail.com>
2026-03-08 17:44:18 +0530

    mar-8-9

---
+++ 9_module/pip1.py
@@ -0,0 +1,12 @@
+
+
+#pip install library_name
+#pip uninstall library_name
+#pip list/freeze
+#pip install library_name==version_number
+#upgrade library
+#pip install --upgrade library_name
+print('This is pip file')
+import time
+import pandas
+import pyotp
