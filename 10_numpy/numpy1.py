a7ed8af5f0414fe4afa278ecd6280eb220c4eed0
sunilsala88 <sunilsala88@gmail.com>
2026-03-14 14:59:18 +0530

    mar-14-2

--- 10_numpy/numpy1.py
+++ 10_numpy/numpy1.py
@@ -1,6 +1,13 @@
 
 import numpy as np
-l1=[1,2,3]
+l1=[1,2,3,4.4]
 print(l1)
 arr=np.array(l1)
 print(arr)
+
+ar1=np.zeros(5,dtype=int)
+print(ar1)
+ar2=np.ones(10,dtype=int)
+print(ar2)
+ar3=np.arange(100,200)
+print(ar3)
