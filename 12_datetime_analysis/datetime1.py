4a4832d5d09ef71b5de2c724b944d7c8e8055347
sunilsala88 <sunilsala88@gmail.com>
2026-03-28 14:53:05 +0530

    mar-28-1

---
+++ 12_datetime_analysis/datetime1.py
@@ -0,0 +1,32 @@
+
+
+import datetime as dt
+import time
+
+d1=dt.date(2025,3,28)
+print(d1)
+
+
+t1=dt.time(11,15,10)
+print(t1)
+
+
+dt1=dt.datetime(2026,3,29,11,15,10)
+print(dt1)
+
+dd1=dt.timedelta(minutes=5)
+print(dd1)
+
+
+
+a=dt1.weekday() # 0-6 (0 is Monday, 6 is Sunday)
+print(a)
+
+
+# start=dt.datetime(2024,1,1)
+# list1=[start]
+# for i in range(365):
+#     start=start + dt.timedelta(days=1)
+#     list1.append(start)
+
+# print(list1)
