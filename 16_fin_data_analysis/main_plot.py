077488dcaedbb262da33b75e2f25f3da284c8fdf
sunilsala88 <sunilsala88@gmail.com>
2026-04-05 15:15:39 +0530

    apr5-8

---
+++ 16_fin_data_analysis/main_plot.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+import datetime as dt
+
+e=dt.datetime.now()
+s=e-dt.timedelta(days=2000)
+print(s,e)
+data=yf.download('TSLA',start=s,end=e,multi_level_index=False,interval='1d',ignore_tz=True )
+print(data)
+
+import mplfinance as mpf
+mpf.plot(data,type='candle',volume=True,style='yahoo')
